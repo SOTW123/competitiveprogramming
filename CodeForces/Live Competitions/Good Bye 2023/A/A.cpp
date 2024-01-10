72b503889c0c2d7bcf9b93dbd21b51e59a043100
SOTW123 <siyamszaman612@gmail.com>
2024-01-10 07:24:06 -0600

    1/10/24 Home -> School

---
+++ CodeForces/Live Competitions/Good Bye 2023/A/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef double long dl;
+
+ifstream fin("A.in");
+
+template<typename T>
+void printV(vector<T> v);
+
+int main() {
+    
+    return 0;
+}
+
+
+template<typename T>
+void printV(vector<T> v) {
+    for(T value : v) {
+        cout << value << " ";
+    }
+    cout << endl;
+}
