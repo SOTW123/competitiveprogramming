6238eefc4ffef74aad52137a3381e50be95192db
SOTW123 <siyamszaman612@gmail.com>
2024-02-06 11:06:09 -0600

    213

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	int t;
+	cin >> t;
+	for(int i = 0; i < t; i++)
+	{
+		string lca = "abcdefghijklmnopqrstuvwxyz"; // lowercase alphabet
+												string uca = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // uppercase alphabet
+		string line;
+		cin >> line;
+		string ans = "";
+		int n = line.size();
+		string curr = "";
+		for(int j = 0; j < n; j++)
+		{
+			curr += line[j];
+			if(line[j] == 'B')
+			{
+				int f = curr.find
+			}
+			else if(line[j] == 'b')
+			{
+
+			}
+			else {
+				ans+=line[j];
+			}
+		}
+	}	
+}
